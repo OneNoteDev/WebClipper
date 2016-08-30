473c45b3e060015903b42ba006ec8a1d06db6914
Matthew Chiam <machiam@microsoft.com>
2016-08-30 09:37:32 -0700

    frontEndGlobals.ts now uses the new storage interface

--- src/scripts/clipperUI/frontEndGlobals.ts
+++ src/scripts/clipperUI/frontEndGlobals.ts
@@ -5,11 +5,17 @@ import {Communicator} from "../communicator/communicator";
 import {Logger} from "../logging/logger";
 import {Constants} from "../constants";
 
+import {RemoteStorage} from "../storage/remoteStorage";
+import {Storage} from "../storage/storage";
+
 export module Clipper {
 	export let injectCommunicator: Communicator;
 	export let extensionCommunicator: Communicator;
 	export let logger: Logger;
 	export let sessionId: SmartValue<string> = new SmartValue<string>();
+
+	// TODO make Clipper a class or something else?
+	export let storage: Storage;
 
 	export function getUserSessionId(): string {
 		return this.sessionId.get();
@@ -17,13 +23,18 @@ export module Clipper {
 
 	export module Storage {
 		export function getValue(key: string, callback: (value: string) => void): void {
-			Clipper.extensionCommunicator.callRemoteFunction(Constants.FunctionKeys.getStorageValue, { param: key, callback: (value: string) => {
-				callback(value);
-			}});
+			// TODO this could be made much safer if this was a class
+			if (!Clipper.storage) {
+				Clipper.storage = new RemoteStorage(Clipper.extensionCommunicator);
+			}
+			Clipper.storage.getValue(key, callback);
 		}
 
 		export function setValue(key: string, value: string): void {
-			Clipper.extensionCommunicator.callRemoteFunction(Constants.FunctionKeys.setStorageValue, { param: { key: key, value: value }});
+			if (!Clipper.storage) {
+				Clipper.storage = new RemoteStorage(Clipper.extensionCommunicator);
+			}
+			Clipper.storage.setValue(key, value, undefined);
 		}
 	}
 }
