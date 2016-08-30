fed884a020ffda6980aa8d4667d6d34d2ce2d288
Matthew Chiam <machiam@microsoft.com>
2016-08-30 09:27:36 -0700

    Implemented remote storage

---
+++ src/scripts/storage/remoteStorage.ts
@@ -0,0 +1,32 @@
+import {Constants} from "../constants";
+
+import {Communicator} from "../communicator/communicator";
+
+import {Storage} from "./storage";
+
+export class RemoteStorage implements Storage {
+	private extensionCommunicator: Communicator;
+
+	constructor(extCommunicator: Communicator) {
+		this.extensionCommunicator = extCommunicator;
+	}
+
+	public getValue(key: string, callback: (value: string) => void): void {
+		this.extensionCommunicator.callRemoteFunction(Constants.FunctionKeys.getStorageValue, { param: key, callback: (value: string) => {
+			callback(value);
+		}});
+	}
+
+	public setValue(key: string, value: string, callback?: (value: string) => void): void {
+		this.extensionCommunicator.callRemoteFunction(Constants.FunctionKeys.setStorageValue, {
+			param: { key: key, value: value }, callback: (retVal: string) => {
+				if (callback) {
+					callback(retVal);
+				}
+		}});
+	}
+
+	public removeKey(key: string, callback?: (successful: boolean) => void): void {
+		// TODO
+	}
+}
