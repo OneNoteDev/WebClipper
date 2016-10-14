be6e2f34c1f61472b8fe6bf3be07975b5b674f60
Matthew Chiam <machiam@microsoft.com>
2016-10-14 13:44:56 -0700

    Added more UTs

--- src/tests/clipperUI/panels/clippingPanelWithDelayedMessage_tests.tsx
+++ src/tests/clipperUI/panels/clippingPanelWithDelayedMessage_tests.tsx
@@ -31,3 +31,30 @@ test("If passed a delay of 0, the panel should display the delayed message as so
 	strictEqual(clipProgressDelayedMessage.innerText, expectedMessage,
 		"The message should be rendered in the clip progress delayed message");
 });
+
+test("If passed a non-zero positive delay, the panel should not display the delayed message until the delay has been passed", () => {
+	let expectedMessage = "hello world";
+	let delay = 10000;
+	HelperFunctions.mountToFixture(<ClippingPanelWithDelayedMessage clipperState={mockClipperState} delay={delay} message={expectedMessage} />);
+
+	let clipProgressDelayedMessage = document.getElementById(Constants.Ids.clipProgressDelayedMessage);
+	ok(!clipProgressDelayedMessage, "The clip progress delayed message should not render immediately");
+
+	clock.tick(delay - 500);
+	ok(!clipProgressDelayedMessage, "The clip progress delayed message should not render just before the delay");
+
+	clock.tick(500);
+	ok(clipProgressDelayedMessage, "The clip progress delayed message should render after the delay");
+	strictEqual(clipProgressDelayedMessage.innerText, expectedMessage,
+		"The message should be rendered in the clip progress delayed message");
+});
+
+test("If passed a delay < 0, the panel should display the delayed message as soon as it is instantiated", () => {
+	let expectedMessage = "hello world";
+	HelperFunctions.mountToFixture(<ClippingPanelWithDelayedMessage clipperState={mockClipperState} delay={-10000} message={expectedMessage} />);
+
+	let clipProgressDelayedMessage = document.getElementById(Constants.Ids.clipProgressDelayedMessage);
+	ok(clipProgressDelayedMessage, "The clip progress delayed message should render immediately");
+	strictEqual(clipProgressDelayedMessage.innerText, expectedMessage,
+		"The message should be rendered in the clip progress delayed message");
+});
