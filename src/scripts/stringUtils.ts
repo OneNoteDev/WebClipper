f5f07ae9c6e54dfb335432e57dac31dd06d70d49
Vishaal Kalwani <vishaalkal@gmail.com>
2016-10-18 11:50:50 -0700

    Completed parsePageRange tests

--- src/scripts/stringUtils.ts
+++ src/scripts/stringUtils.ts
@@ -1,20 +1,46 @@
+import { Utils } from "./utils";
+import * as _ from "lodash";
+
 export module StringUtils {
+
 	// Takes a range of the form 1,3-6,7,8,13,1,3,4,a-b, etc. and then returns an array
 	// corresponding to the numbers in that range. It ignores invalid input, sorts it, and removes duplicates
 	export function parsePageRange(text: string): number[] {
-		let initialRange = text.split(",").reduce((previousValue, currentValue) => {
+		if (Utils.isNullOrUndefined(text)) {
+			return;
+		}
+
+		text = text.trim();
+
+		if (text === "") {
+			return [];
+		}
+
+		let splitText = text.split(",");
+		let range: number[] = [];
+
+		for (let i = 0; i < splitText.length; ++i) {
 			let valueToAppend: number[] = [], matches;
-			// The value could be a single digit
+			let currentValue = splitText[i].trim();
+
 			if (/^\d+$/.test(currentValue)) {
 				valueToAppend = [parseInt(currentValue, 10 /* radix */)];
 				// ... or it could a range of the form [#]-[#]
-			} else if (matches = /^(\d+)-(\d+)$/.exec(currentValue)) {
+			} else if (matches = /^(\d+)\s*-\s*(\d+)$/.exec(currentValue)) {
 				let lhs = parseInt(matches[1], 10), rhs = parseInt(matches[2], 10) + 1;
-				// TODO: what do we do if start > end? This is a behavior question, not an implementation one
+				// Disallow ranges like 5-3, or 10-1
+				if (lhs >= rhs) {
+					return undefined;
+				}
 				valueToAppend = _.range(lhs, rhs);
+			} else {
+				// The currentValue is not a single digit or a valid range
+				return undefined;
 			}
-			return previousValue = previousValue.concat(valueToAppend);
-		}, []);
-		return _(initialRange).sortBy().sortedUniq().map((page) => { return page - 1; }).value();
+
+			range = range.concat(valueToAppend);
+		}
+
+		return _(range).sortBy().sortedUniq().value();
 	}
 }
