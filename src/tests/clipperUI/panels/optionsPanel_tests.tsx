e0292213189a9451ee7a3cd8ae406e2e24a43f24
Matthew Chiam <machiam@microsoft.com>
2016-10-31 16:38:08 -0700

    More UTs for the full page screenshot scenarios

--- src/tests/clipperUI/panels/optionsPanel_tests.tsx
+++ src/tests/clipperUI/panels/optionsPanel_tests.tsx
@@ -43,3 +43,45 @@ test("If the current mode is full page and its call to the API has succeeded, th
 
 	ok(onStartClipCalled, "The onStartClip callback should be called");
 });
+
+test("If the current mode is full page and its call to the API is in progress, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.FullPage);
+	mockOptionsPanelProps.clipperState.fullPageResult.status = Status.InProgress;
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	let clipButton = document.getElementById(Constants.Ids.clipButton);
+	ok(clipButton, "The clip button should be present");
+
+	let clipButtonContainer = document.getElementById(Constants.Ids.clipButtonContainer);
+	ok(clipButtonContainer, "The clip button container should be present");
+	ok(clipButtonContainer.classList.contains("disabled"),
+		"The clip button container should indicate that the button is inactive");
+
+	HelperFunctions.simulateAction(() => {
+		clipButton.click();
+	});
+
+	ok(!onStartClipCalled, "The onStartClip callback should not be called");
+});
+
+test("If the current mode is full page and its call to the API failed, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.FullPage);
+	mockOptionsPanelProps.clipperState.fullPageResult.status = Status.Failed;
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	let clipButton = document.getElementById(Constants.Ids.clipButton);
+	ok(clipButton, "The clip button should be present");
+
+	let clipButtonContainer = document.getElementById(Constants.Ids.clipButtonContainer);
+	ok(clipButtonContainer, "The clip button container should be present");
+	ok(clipButtonContainer.classList.contains("disabled"),
+		"The clip button container should indicate that the button is inactive");
+
+	HelperFunctions.simulateAction(() => {
+		clipButton.click();
+	});
+
+	ok(!onStartClipCalled, "The onStartClip callback should not be called");
+});
