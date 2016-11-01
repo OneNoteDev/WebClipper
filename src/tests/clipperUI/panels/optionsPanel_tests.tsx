a9edefcdccfb599e3398ae6d511d22c3a900b096
Matthew Chiam <machiam@microsoft.com>
2016-11-01 09:56:47 -0700

    More test cases

--- src/tests/clipperUI/panels/optionsPanel_tests.tsx
+++ src/tests/clipperUI/panels/optionsPanel_tests.tsx
@@ -6,6 +6,8 @@ import {Status} from "../../../scripts/clipperUI/status";
 
 import {OptionsPanel, OptionsPanelProp} from "../../../scripts/clipperUI/panels/optionsPanel";
 
+import {AugmentationModel} from "../../../scripts/contentCapture/augmentationHelper";
+
 import {HelperFunctions} from "../../helperFunctions";
 
 let mockOptionsPanelProps: OptionsPanelProp;
@@ -23,25 +25,29 @@ QUnit.module("optionsPanel", {
 	}
 });
 
+function assertClipButtonAvailability(isAvailable: boolean) {
+	let clipButton = document.getElementById(Constants.Ids.clipButton);
+	ok(clipButton, "The clip button should be present");
+
+	let clipButtonContainer = document.getElementById(Constants.Ids.clipButtonContainer);
+	ok(clipButtonContainer, "The clip button container should always be present");
+	strictEqual(!clipButtonContainer.classList.contains("disabled"), isAvailable,
+		"The clip button container should indicate that the button is active if it is available");
+
+	HelperFunctions.simulateAction(() => {
+		clipButton.click();
+	});
+
+	strictEqual(onStartClipCalled, isAvailable, "The onStartClip callback should be called only if it's available");
+}
+
 test("If the current mode is full page and its call to the API has succeeded, the clip button should be active and clickable", () => {
 	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.FullPage);
 	mockOptionsPanelProps.clipperState.fullPageResult.status = Status.Succeeded;
 	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
 		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
 
-	let clipButton = document.getElementById(Constants.Ids.clipButton);
-	ok(clipButton, "The clip button should be present");
-
-	let clipButtonContainer = document.getElementById(Constants.Ids.clipButtonContainer);
-	ok(clipButtonContainer, "The clip button container should be present");
-	ok(!clipButtonContainer.classList.contains("disabled"),
-		"The clip button container should indicate that the button is active");
-
-	HelperFunctions.simulateAction(() => {
-		clipButton.click();
-	});
-
-	ok(onStartClipCalled, "The onStartClip callback should be called");
+	assertClipButtonAvailability(true);
 });
 
 test("If the current mode is full page and its call to the API is in progress, the clip button should not be active or clickable", () => {
@@ -50,19 +56,7 @@ test("If the current mode is full page and its call to the API is in progress, t
 	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
 		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
 
-	let clipButton = document.getElementById(Constants.Ids.clipButton);
-	ok(clipButton, "The clip button should be present");
-
-	let clipButtonContainer = document.getElementById(Constants.Ids.clipButtonContainer);
-	ok(clipButtonContainer, "The clip button container should be present");
-	ok(clipButtonContainer.classList.contains("disabled"),
-		"The clip button container should indicate that the button is inactive");
-
-	HelperFunctions.simulateAction(() => {
-		clipButton.click();
-	});
-
-	ok(!onStartClipCalled, "The onStartClip callback should not be called");
+	assertClipButtonAvailability(false);
 });
 
 test("If the current mode is full page and its call to the API failed, the clip button should not be active or clickable", () => {
@@ -71,17 +65,134 @@ test("If the current mode is full page and its call to the API failed, the clip 
 	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
 		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
 
-	let clipButton = document.getElementById(Constants.Ids.clipButton);
-	ok(clipButton, "The clip button should be present");
+	assertClipButtonAvailability(false);
+});
 
-	let clipButtonContainer = document.getElementById(Constants.Ids.clipButtonContainer);
-	ok(clipButtonContainer, "The clip button container should be present");
-	ok(clipButtonContainer.classList.contains("disabled"),
-		"The clip button container should indicate that the button is inactive");
+test("If the current mode is region and there are regions selected, the clip button should be active and clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Region);
+	mockOptionsPanelProps.clipperState.regionResult.status = Status.Succeeded;
+	mockOptionsPanelProps.clipperState.regionResult.data = ["src1", "src2"];
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
 
-	HelperFunctions.simulateAction(() => {
-		clipButton.click();
-	});
+	assertClipButtonAvailability(true);
+});
 
-	ok(!onStartClipCalled, "The onStartClip callback should not be called");
+test("If the current mode is region and there are no regions selected, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Region);
+	mockOptionsPanelProps.clipperState.regionResult.status = Status.Succeeded;
+	mockOptionsPanelProps.clipperState.regionResult.data = [];
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
+});
+
+test("If the current mode is region and the status is NotStarted, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Region);
+	mockOptionsPanelProps.clipperState.regionResult.status = Status.NotStarted;
+	mockOptionsPanelProps.clipperState.regionResult.data = [];
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
+});
+
+test("If the current mode is region and the status is InProgress, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Region);
+	mockOptionsPanelProps.clipperState.regionResult.status = Status.InProgress;
+	mockOptionsPanelProps.clipperState.regionResult.data = [];
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
+});
+
+test("If the current mode is region and the status is InProgress and has images already stored, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Region);
+	mockOptionsPanelProps.clipperState.regionResult.status = Status.InProgress;
+	mockOptionsPanelProps.clipperState.regionResult.data = ["src1", "src2"];
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
+});
+
+test("If the current mode is selection and the status is succeeded and has a text stored in clipperState, the clip button should be active and clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Selection);
+	mockOptionsPanelProps.clipperState.selectionPreviewInfo.previewBodyHtml = "hello world";
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(true);
+});
+
+test("If the current mode is augmentation and the status is succeeded and has content stored in clipperState, the clip button should be active and clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Augmentation);
+	mockOptionsPanelProps.clipperState.augmentationResult.status = Status.Succeeded;
+	mockOptionsPanelProps.clipperState.augmentationResult.data = {
+		ContentInHtml: "hello article",
+		ContentModel: AugmentationModel.Article
+	};
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(true);
+});
+
+test("If the current mode is augmentation and the status is succeeded and has content stored in clipperState as a recipe, the clip button should be active and clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Augmentation);
+	mockOptionsPanelProps.clipperState.augmentationResult.status = Status.Succeeded;
+	mockOptionsPanelProps.clipperState.augmentationResult.data = {
+		ContentInHtml: "hello article",
+		ContentModel: AugmentationModel.Recipe
+	};
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(true);
+});
+
+test("If the current mode is augmentation and the status is succeeded but no content was found, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Augmentation);
+	mockOptionsPanelProps.clipperState.augmentationResult.status = Status.Succeeded;
+	mockOptionsPanelProps.clipperState.augmentationResult.data = {
+		ContentInHtml: "",
+		ContentModel: AugmentationModel.None
+	};
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
+});
+
+test("If the current mode is augmentation and the status is succeeded but no content was found, and ContentInHtml is undefined, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Augmentation);
+	mockOptionsPanelProps.clipperState.augmentationResult.status = Status.Succeeded;
+	mockOptionsPanelProps.clipperState.augmentationResult.data = {
+		ContentInHtml: undefined,
+		ContentModel: AugmentationModel.None
+	};
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
+});
+
+test("If the current mode is augmentation and the status is in progress, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Augmentation);
+	mockOptionsPanelProps.clipperState.augmentationResult.status = Status.InProgress;
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
+});
+
+test("If the current mode is augmentation and the status indicates failure, the clip button should not be active or clickable", () => {
+	mockOptionsPanelProps.clipperState.currentMode.set(ClipMode.Augmentation);
+	mockOptionsPanelProps.clipperState.augmentationResult.status = Status.Failed;
+	HelperFunctions.mountToFixture(<OptionsPanel onStartClip={mockOptionsPanelProps.onStartClip}
+		onPopupToggle={mockOptionsPanelProps.onPopupToggle} clipperState={mockOptionsPanelProps.clipperState} />);
+
+	assertClipButtonAvailability(false);
 });
