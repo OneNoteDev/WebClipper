4efc2110859f809c59510ddc9dd5c7efde8f6b34
Matthew Chiam <machiam@microsoft.com>
2016-11-09 15:09:33 -0800

    Added more test setup code

--- src/tests/saveToOneNote/saveToOneNote_tests.ts
+++ src/tests/saveToOneNote/saveToOneNote_tests.ts
@@ -1,13 +1,32 @@
 import * as sinon from "sinon";
 
+import {MockPdfDocument} from "../contentCapture/mockPdfDocument";
+
+import {SaveToOneNote} from "../../scripts/saveToOneNote/saveToOneNote";
+import {OneNoteSaveablePage} from "../../scripts/saveToOneNote/oneNoteSaveablePage";
+import {OneNoteSaveablePdf} from "../../scripts/saveToOneNote/oneNoteSaveablePdf";
+
+function getMockSaveablePage(): OneNoteSaveablePage {
+	let page = new OneNoteApi.OneNotePage();
+	return new OneNoteSaveablePage(page);
+}
+
+function getMockSaveablePdf(pageIndexes?: number[]): OneNoteSaveablePdf {
+	let page = new OneNoteApi.OneNotePage();
+	let mockPdf = new MockPdfDocument();
+	return new OneNoteSaveablePdf(page, mockPdf, pageIndexes);
+}
+
 let xhr: Sinon.SinonFakeXMLHttpRequest;
 let server: Sinon.SinonFakeServer;
+let saveToOneNote: SaveToOneNote;
 
 QUnit.module("saveToOneNote-sinon", {
 	beforeEach: () => {
 		xhr = sinon.useFakeXMLHttpRequest();
 		server = sinon.fakeServer.create();
 		server.respondImmediately = true;
+		saveToOneNote = new SaveToOneNote("userToken");
 	},
 	afterEach: () => {
 		xhr.restore();
@@ -19,4 +38,16 @@ test("hello world", (assert: QUnitAssert) => {
 	let done = assert.async();
 	ok(true);
 	done();
+});
+
+// Some saveToOneNote save() calls make multiple, different calls to the API
+QUnit.module("saveToOneNote-sinon-multiResponse", {
+	beforeEach: () => {
+		xhr = sinon.useFakeXMLHttpRequest();
+		server = sinon.fakeServer.create();
+	},
+	afterEach: () => {
+		xhr.restore();
+		server.restore();
+	}
 });
