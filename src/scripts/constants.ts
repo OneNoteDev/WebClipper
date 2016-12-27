bf466fbff3526832a145a8a305ac5e47b224ed6f
Chad Shurtz <chadsh@microsoft.com>
2016-12-27 11:09:08 -0800

    Add in the new constants for the sign-in panel.

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -222,6 +222,8 @@ export module Constants {
 		export var signInButtonMsa = "signInButtonMsa";
 		export var signInButtonOrgId = "signInButtonOrgId";
 		export var signInContainer = "signInContainer";
+		export var signInErrorDebugInformation = "signInErrorDebugInformation";
+		export var signInErrorDebugInformationDescription = "signInErrorDebugInformationDescription";
 		export var signInErrorDebugInformationContainer = "signInErrorDebugInformationContainer";
 		export var signInErrorDebugInformationList = "signInErrorDebugInformationList";
 		export var signInErrorDescription = "signInErrorDescription";
@@ -345,7 +347,6 @@ export module Constants {
 	}
 
 	export module Urls {
-		// export var serviceDomain = "https://minint-per7o4f.redmond.corp.microsoft.com";
 		export var serviceDomain = "https://www.onenote.com";
 
 		export var augmentationApiUrl = serviceDomain + "/onaugmentation/clipperextract/v1.0/";
