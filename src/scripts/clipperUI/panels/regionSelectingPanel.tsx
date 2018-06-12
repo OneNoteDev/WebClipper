91ce9e224c90fb003d5f8b933b00bdc8b4935aeb
v-janwil <iamjane@gmail.com>
2018-06-12 16:03:25 -0700

    add offset borders to focused elements on dark backgrounds

--- src/scripts/clipperUI/panels/regionSelectingPanel.tsx
+++ src/scripts/clipperUI/panels/regionSelectingPanel.tsx
@@ -20,13 +20,13 @@ class RegionSelectingPanelClass extends ComponentBase<{}, ClipperStateProp> {
 							{Localization.getLocalizedString("WebClipper.Label.DragAndRelease")}
 						</span>
 					</div>
+					<div className="wideButtonContainer">
 					<a id={Constants.Ids.regionClipCancelButton} {...this.enableInvoke({callback: this.handleCancelButton, tabIndex: 70})}>
-						<div className="wideButtonContainer">
 							<span className="wideButtonFont wideActionButton" style={Localization.getFontFamilyAsStyle(Localization.FontFamily.Semibold)}>
 								{Localization.getLocalizedString("WebClipper.Action.BackToHome")}
 							</span>
-						</div>
 					</a>
+					</div>
 				</div>
 			</div>
 		);
