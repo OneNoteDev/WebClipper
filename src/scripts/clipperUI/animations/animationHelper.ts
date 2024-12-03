408be0e1565ecc9f7d518dc9baf34db16904d92a
Arpan Kundu <arpankundu@microsoft.com>
2024-12-03 20:12:54 +0530

    panel animation fix

--- src/scripts/clipperUI/animations/animationHelper.ts
+++ src/scripts/clipperUI/animations/animationHelper.ts
@@ -2,9 +2,7 @@ declare var Velocity: jquery.velocity.VelocityStatic;
 
 export class AnimationHelper {
 	public static stopAnimationsThen(el: HTMLElement, callback: () => void) {
-		Velocity.animate(el, "stop", {
-			queue: true,
-		});
+		Velocity.animate(el, "stop", true as any);
 		setTimeout(callback, 1);
 	}
 }
