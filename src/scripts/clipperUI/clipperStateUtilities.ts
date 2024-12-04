efcf3bad63587284791786b776875a870041657a
Arpan Kundu <arpankundu@microsoft.com>
2024-12-05 03:51:21 +0530

    idle auth fix

--- src/scripts/clipperUI/clipperStateUtilities.ts
+++ src/scripts/clipperUI/clipperStateUtilities.ts
@@ -1,13 +1,63 @@
-import {StringUtils} from "../stringUtils";
 import {ObjectUtils} from "../objectUtils";
 
+import * as Log from "../logging/log";
 import {ClipMode} from "./clipMode";
 import {ClipperState} from "./clipperState";
 import {Status} from "./status";
+import { Clipper } from "./frontEndGlobals";
+import { SmartValue } from "../communicator/smartValue";
+import { Constants } from "../constants";
+import { TimeStampedData, CachedHttp } from "../http/cachedHttp";
+import { UserInfo, UpdateReason } from "../userInfo";
 
 export module ClipperStateUtilities {
-	export function isUserLoggedIn(state: ClipperState): boolean {
-		return (state.userResult && state.userResult.status && state.userResult.data && !!state.userResult.data.user);
+	export function isUserLoggedIn(state: ClipperState, refreshUserInfo?: boolean): boolean {
+		if (state.userResult && state.userResult.status && state.userResult.data && !!state.userResult.data.user) {
+			return true;
+		} else if (!!refreshUserInfo) {
+			let userInfoUpdateCb = (updatedUser: UserInfo) => {
+				console.log(updatedUser);
+				if (updatedUser) {
+					let userInfoUpdatedEvent = new Log.Event.BaseEvent(Log.Event.Label.UserInfoUpdated);
+					userInfoUpdatedEvent.setCustomProperty(Log.PropertyName.Custom.UserUpdateReason, UpdateReason[updatedUser.updateReason]);
+					userInfoUpdatedEvent.setCustomProperty(Log.PropertyName.Custom.LastUpdated, new Date(updatedUser.lastUpdated).toUTCString());
+					Clipper.logger.logEvent(userInfoUpdatedEvent);
+				}
+
+				if (updatedUser && updatedUser.user) {
+					let timeStampedData: TimeStampedData = {
+						data: updatedUser.user,
+						lastUpdated: updatedUser.lastUpdated
+					};
+
+					// The user SV should never be set with expired user information
+					let tokenHasExpiredForLoggedInUser = CachedHttp.valueHasExpired(timeStampedData, (updatedUser.user.accessTokenExpiration * 1000) - 180000);
+					if (tokenHasExpiredForLoggedInUser) {
+						Clipper.logger.logFailure(Log.Failure.Label.UserSetWithInvalidExpiredData, Log.Failure.Type.Unexpected);
+					}
+
+					state.setState({ userResult: { status: Status.Succeeded, data: updatedUser } });
+					Clipper.logger.setContextProperty(Log.Context.Custom.AuthType, updatedUser.user.authType);
+					Clipper.logger.setContextProperty(Log.Context.Custom.UserInfoId, updatedUser.user.cid);
+				} else {
+					state.setState({ userResult: { status: Status.Failed, data: updatedUser } });
+				}
+			};
+
+			state.setState({ userResult: { status: Status.InProgress } });
+			Clipper.getExtensionCommunicator().callRemoteFunction(Constants.FunctionKeys.getInitialUser, {
+				callback: (freshInitialUser: UserInfo) => {
+					if (freshInitialUser && freshInitialUser.user) {
+						Clipper.logger.logUserFunnel(Log.Funnel.Label.AuthAlreadySignedIn);
+					} else if (!freshInitialUser) {
+						userInfoUpdateCb(freshInitialUser);
+					}
+					Clipper.getExtensionCommunicator().subscribeAcrossCommunicator(new SmartValue<UserInfo>(), Constants.SmartValueKeys.user, (updatedUser: UserInfo) => {
+						userInfoUpdateCb(updatedUser);
+					});
+				}
+			});
+		}
 	}
 
 	export function isMsaUser(state: ClipperState): boolean {
