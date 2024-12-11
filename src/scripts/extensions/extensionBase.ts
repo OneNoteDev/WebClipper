4ffc88dd25b0e9a9d93355cc7722afba8c7106a4
Arpan Kundu <arpankundu@microsoft.com>
2024-12-11 18:29:42 +0530

    version update for completion of MV3 migration

--- src/scripts/extensions/extensionBase.ts
+++ src/scripts/extensions/extensionBase.ts
@@ -41,7 +41,7 @@ export abstract class ExtensionBase<TWorker extends ExtensionWorkerBase<TTab, TT
 	protected auth: AuthenticationHelper;
 	protected tooltip: TooltipHelper;
 	protected clientInfo: SmartValue<ClientInfo>;
-	protected static version = "3.9.8";
+	protected static version = "3.9.9";
 
 	constructor(clipperType: ClientType, clipperData: ClipperData) {
 		this.setUnhandledExceptionLogging();
